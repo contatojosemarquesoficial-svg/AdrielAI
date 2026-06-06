fd73552700bbbda4fecaa597339a4931020e9210
contatojosemarquesoficial-svg <contatojosemarquesoficial@gmail.com>
2026-06-06 03:27:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
 import streamlit as st
 import google.generativeai as genai
-
-st.title("AdrielAI - Em funcionamento!")
-st.write("O projeto está configurado corretamente.")
+st.write("O sistema está rodando!")
